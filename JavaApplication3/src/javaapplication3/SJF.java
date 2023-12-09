d0817d5e6bda835dd2af8707dca69e3c927f5eee
Ahmed Ashrf <loginapp2003@gmail.com>
2023-12-09 22:23:51 +0200

    Create SJF.java

---
+++ JavaApplication3/src/javaapplication3/SJF.java
@@ -0,0 +1,21 @@
+package javaapplication3;
+import java.util.ArrayList;
+
+public class SJF {
+    private ArrayList<Process> processes;
+    private ArrayList<Process> readyQueue = new ArrayList<Process>() ;
+
+    SJF(ArrayList<Process> processes)
+    {
+        this.processes = processes ; 
+    }
+
+    public void Scheduling()
+    {
+        
+    }
+    
+
+
+    
+}
