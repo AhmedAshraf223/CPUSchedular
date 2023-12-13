cc6489a660745cc32342493352996926676292de
Ahmed Ashrf <loginapp2003@gmail.com>
2023-12-13 20:27:35 +0200

    Update AGShceduling.java

--- JavaApplication3/src/javaapplication3/AGShceduling.java
+++ JavaApplication3/src/javaapplication3/AGShceduling.java
@@ -5,13 +5,13 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
-
+import java.util.Deque;
 
 
 public class AGShceduling {
     private ArrayList<Process> processes;
   //  private ArrayList<Process> readyQueue = new ArrayList<Process>() ;
-    Queue<Process> readyQueue = new LinkedList<>();
+    Deque<Process> readyQueue = new LinkedList<>();
     private int curr =  0 ;
     private ArrayList<String> timeline = new ArrayList<>() ;
 
@@ -116,7 +116,7 @@ public class AGShceduling {
     private void senario1(Process p)
     {
         float mean = meanOfQuantum() ;
-        p.setQuantum((int)Math.ceil(10 % mean));
+        p.setQuantum((int)Math.ceil((.1 * mean )+ p.getQuantum()));
     }
     // senario 2 when the running process didn't use all it's quantum time ;
     private void senario2(Process p , int counter )
@@ -136,6 +136,39 @@ public class AGShceduling {
         }
     }
 
+    private void deleteProceess(Process p )
+    {
+        for (int i = 0 ; i < processes.size() ; i ++)
+        {
+            if(processes.get(i).getName() == p.getName())
+            {
+                processes.remove(i) ; 
+                break ;
+            }
+        }
+    }
+
+
+    private static boolean searchQueue(Deque<Process> queue, Process p ) {
+        for (Process element : queue) {
+            if (element.getName() == p.getName()) {
+                return true; 
+            }
+        }
+        return false; // Element not found
+    }
+      
+    private  Process searchLeastAGQueue(Deque<Process> queue) {
+        Process temp = queue.getFirst();
+        for (Process element : queue) {
+            if (temp.getAG() < element.getAG()) {
+                temp = element ;
+            }
+        }
+        return temp; // Element not found
+    }
+
+
     public void sheduling()
     {
        
@@ -144,24 +177,64 @@ public class AGShceduling {
         int counter = 1 ;
         // when starting the process ; 
         index = getLeastArrivalTime() ;
-        readyQueue.offer(processes.get(index)) ; 
-        processes.get(index).startTime = curr ;
+        readyQueue.addFirst(processes.get(index)) ; 
+        readyQueue.peekFirst().startTime = curr  ;
         while(true)
         {
-            for( ; counter <=processes.get(index).getQuantum() ; counter ++)
+            for( ; counter <=readyQueue.peekFirst().getQuantum() ; counter ++)
             {
                 curr ++ ;
+                readyQueue.peekFirst().remainingTime -- ; 
+                if(readyQueue.peekFirst().remainingTime == 0 )
+                {
+                    readyQueue.peekFirst().finishTime = curr ;
+                    readyQueue.peekFirst().setBurst(0);
+                    deleteProceess(readyQueue.peekFirst());
+                    readyQueue.removeFirst() ; 
+                }
+
+
                 // check if the currend burst time == ceil of 50 of total burst time
-                if(counter >= Math.ceil(processes.get(index).getQuantum() / 2))
+                if(counter >= Math.ceil(readyQueue.peekFirst().getBurst()/2))
                 {
                     
                     // check if there are any process that has AG_factor less than the current process
                     AtomicInteger INDEX = new AtomicInteger(index) ;
-                    if(switching(processes.get(index), INDEX))
+                    if(switching(readyQueue.peekFirst(), INDEX))
                     {
+                        // senario one if process used all its quantum time
+                        if(counter == readyQueue.peekFirst().getQuantum())
+                        {
+                            senario1(readyQueue.peekFirst());
+                        }
+                        // senario two if the process don't use all of it's quantum
+                        else if(counter < readyQueue.peekFirst().getQuantum())
+                        {
+                            senario2(readyQueue.peekFirst(), counter);
+                        }
                         index = INDEX.get() ;
-                        processes.get(index).startTime = curr ;
-                        counter = 0  ; 
+
+                        // in case if the process not in ready queue unitil yet
+                        // add the new process to the queue
+                        if(!searchQueue(readyQueue, processes.get(index)))
+                        {
+                            Process temp  = readyQueue.peekFirst() ;
+                            readyQueue.removeFirst() ; 
+                            readyQueue.addFirst(processes.get(index)) ;
+                            readyQueue.addLast(temp);
+                        }
+
+                        // in case the process in the ready qeueu ; 
+                        else if (searchQueue(readyQueue, processes.get(index)))
+                        {
+                            Process temp = searchLeastAGQueue(readyQueue) ; 
+                            readyQueue.remove(temp);
+                            readyQueue.addLast(readyQueue.getFirst());
+                            readyQueue.removeFirst(); 
+                            readyQueue.addFirst(temp); 
+                        }
+
+                       
                     }
                 }
             }
