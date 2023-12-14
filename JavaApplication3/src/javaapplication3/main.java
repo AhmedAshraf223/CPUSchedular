43a4d83be51bf588fec82ac140f6cc6d4f6ae740
Ahmed Ashrf <loginapp2003@gmail.com>
2023-12-14 19:21:29 +0200

    Update main.java

--- JavaApplication3/src/javaapplication3/main.java
+++ JavaApplication3/src/javaapplication3/main.java
@@ -1,4 +1,4 @@
-package javaapplication3;
+//package javaapplication3;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,36 +6,41 @@ import java.util.Scanner;
 public class main {
     static public void main(String[] args)
     {
-        int counter ; 
+        //int counter ; 
         ArrayList<Process> Processes = new ArrayList<Process>();
-		Scanner input = new Scanner(System.in);
-        System.out.println("Enter n of processes :");
-		counter = input.nextInt() ;
+		// Scanner input = new Scanner(System.in);
+        // System.out.println("Enter n of processes :");
+		// counter = input.nextInt() ;
 
-		System.out.println("Enter Quantum value :");
-		int quantum = input.nextInt() ;
+		// System.out.println("Enter Quantum value :");
+		// int quantum = input.nextInt() ;
 
-        for(int i = 0 ; i < counter ;i++)
-		{
-			input = new Scanner(System.in);
-			Process p = new Process();
-			System.out.println( (i+1) + " Enter Process Name :");
-			p.setName(input.nextLine());
+        // for(int i = 0 ; i < counter ;i++)
+		// {
+		// 	input = new Scanner(System.in);
+		// 	Process p = new Process();
+		// 	System.out.println( (i+1) + " Enter Process Name :");
+		// 	p.setName(input.nextLine());
 		
-			System.out.println("Enter Process Burst Time :");
-			p.setBurst(input.nextInt());
+		// 	System.out.println("Enter Process Burst Time :");
+		// 	p.setBurst(input.nextInt());
 
-			System.out.println("Enter Process Arraival Time :");
-			p.setArrivalTime(input.nextInt()) ;
+		// 	System.out.println("Enter Process Arraival Time :");
+		// 	p.setArrivalTime(input.nextInt()) ;
 			
-			// System.out.println("Enter Process priority :");
-			// p.setPriorty(input.nextInt()) ;
-			p.setQuantum(quantum);
-			p.setRemainingTime(p.getBurst());
-			Processes.add(p);	
-		}
+		// 	System.out.println("Enter Process priority :");
+		// 	p.setPriorty(input.nextInt()) ;
+		// 	p.setQuantum(quantum);
+		// 	p.setRemainingTime(p.getBurst());
+		// 	Processes.add(p);	
+		// }
+		Process p1 = new Process("p1" , 0 , 17 , 4 , 4  ) ;
+		Process p2 = new Process("p2" , 3 , 6 , 9 , 4  ) ;
+		Process p3 = new Process("p3" , 4 , 10 , 2 , 4 ) ;
+		Process p4 = new Process("p4" , 29 , 4 , 8 , 4 ) ;
+		Processes.add(p1) ; Processes.add(p2) ; Processes.add(p3) ; Processes.add(p4) ; 
         AGShceduling algo = new AGShceduling(Processes);
-        algo.sheduling();; 
+        algo.sheduling(); 
 		
 		algo.getResults();
 
