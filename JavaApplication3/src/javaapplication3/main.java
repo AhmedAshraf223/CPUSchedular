6dbd95d77020fb2f8e4abdcf4343e6551325cf6a
Ahmed Ashrf <loginapp2003@gmail.com>
2023-12-14 20:30:08 +0200

    Update main.java

--- JavaApplication3/src/javaapplication3/main.java
+++ JavaApplication3/src/javaapplication3/main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class main {
     static public void main(String[] args)
     {
+		System.out.println((int)Math.ceil(2.5));
         //int counter ; 
         ArrayList<Process> Processes = new ArrayList<Process>();
 		// Scanner input = new Scanner(System.in);
